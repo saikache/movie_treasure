0154af48138c8c4f051f82adc7ae6bd8dc6e43a9
sai <sai@sai-300E4Z-300E5Z-300E7Z.(none)>
2014-08-14 13:33:33 +0530

    movie search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieTreasure::Application.config.secret_key_base = '0fb4efe12b6617d55736c63d22a936f1afc5433c46ff491890b8f696dc72e8cd0f1e4cb5f91cece649948b5c9924478d4d28d7ffcfb6306ce58bce7a3348fb44'
