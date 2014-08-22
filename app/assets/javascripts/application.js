a0b7c135b7bc1be060b0bc9761898f5458657050
sai <saisagarkache@gmail.com>
2014-08-22 12:14:25 +0530

    gem foundation pages styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
